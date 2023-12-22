fbdc5bd59ce49eea0c428476b5b78f36fcb557f4
Aditya Prajapati <adityaprajapati28@gmail.com>
2023-12-22 23:33:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { About, Footer, Header, Skills, Testimonials, Work } from "./container/index.js";
+import { Navbar } from "./components/index.js";
+import "./App.scss"
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
