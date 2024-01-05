b0f2f6c8372571c138ebb88677e924e7d91ba275
Aditya Prajapati <adityaprajapati28@gmail.com>
2024-01-05 23:06:56 +0530

    Testimonial and Contact section completed

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./About.scss";
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { motion } from "framer-motion";
 import { images } from "../../constants";
 
@@ -36,4 +36,4 @@ const About = () => {
   )
 }
 
-export default AppWrap(About, "about");
+export default AppWrap(MotionWrap(About, "app__about"), "about", "app__whitebg");
